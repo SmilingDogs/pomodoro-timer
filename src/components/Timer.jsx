9af50261a53187ce0ce833d3338b80381321d049
Smiling Dog <a.kudr74@gmail.com>
2023-08-13 01:42:42 +0300

    clean-up of console.logs

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -6,10 +6,10 @@ import { useContext, useState} from "react";
 import { SettingsContext } from "../context/SettingsProvider";
 import ResetButton from "./ResetButton";
 
-const red = "#f93831";
-const green = "#4aec8c";
+const red = '#f93831';
+const green = '#4aec8c';
 
-function Timer() {
+const Timer = () => {
   const { workMinutes, breakMinutes, setShowSettings, startTimer, setStartTimer} = useContext(SettingsContext);
 
   const [qty, setQty] = useState(0); 
@@ -17,7 +17,6 @@ function Timer() {
   const [timerFinished, setTimerFinished] = useState(false);  
 
   const onTimerComplete = () => {
-
     if (qty < 1) {
       setMode('break');
     }
@@ -28,24 +27,17 @@ function Timer() {
     }
   };
 
-  let isWork = mode === "work";
+  let isWork = mode === 'work';
 
   const renderTime = ({ remainingTime }) => {
     let minutes = Math.floor(remainingTime / 60);
     let seconds = remainingTime % 60;
-    // console.log('Inside renderTime ', remainingTime);
     if (seconds < 10 ) {
-      seconds = '0' + seconds
+      seconds = '0' + seconds;
     }
 
     return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          color: `${isWork ? red : green}`,
-        }}
-      >
+      <div style={{display: 'flex', alignItems: 'center', color: `${isWork ? red : green}`}}>
         <span style={{ fontSize: "4rem", fontWeight: "bold" }}>{minutes}</span>
         <span style={{ fontSize: "3rem", fontWeight: "bold" }}>:</span>
         <span style={{ fontSize: "4rem", fontWeight: "bold" }}>{seconds}</span>
@@ -54,8 +46,8 @@ function Timer() {
   };
 
   return (
-    <div className="timer-wrapper">
-      <h2 className={isWork ? "" : "rest"}>{isWork ? "Work" : "Rest"} </h2>
+    <div className='timer-wrapper'>
+      <h2 className={isWork ? '' : 'rest'}>{isWork ? 'Work' : 'Rest'} </h2>
 
       <CountdownCircleTimer
         key={isWork ? 1 : 2}
@@ -63,19 +55,18 @@ function Timer() {
         duration={(isWork ? workMinutes : breakMinutes) * 60}
         colors={isWork ? red : green}
         strokeWidth={6}
-        trailColor="#151932"
+        trailColor='#151932'
         size={220}
         onComplete={onTimerComplete}
-
       >
         {renderTime}
       </CountdownCircleTimer>
-      <div style={{ marginTop: "30px" }}>
+      <div style={{ marginTop: '30px' }}>
         {!startTimer && !timerFinished && <PlayButton onClick={() => setStartTimer(true)} />}
         {startTimer && !timerFinished && <PauseButton onClick={() => setStartTimer(false)} />}
         {!startTimer && timerFinished && <ResetButton onClick={() => window.location.reload()} />}
       </div>
-      <div style={{ marginTop: "20px" }}>
+      <div style={{ marginTop: '20px' }}>
         <SettingsButton onClick={() => setShowSettings(true)} />
       </div>
     </div>
